cb8c4ba600c2855e21aad61cf774f847e7e5347c
Freddie Pike <freddiepike709@gmail.com>
2024-05-09 03:43:02 -0230

    Exercise 2 (#2)
    * exercise 1 complete
    
    * guess list

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessingInput from '../GuessingInput/GuessingInput';
+import GuessList from '../GuessList/GuessList';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,7 +11,18 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <GuessingInput />;
+  const [guesses, setGuesses] = React.useState([]);
+
+  const updateGuesses = (newGuess) => {
+    const newGuesses = [...guesses];
+    newGuesses.push(newGuess);
+    setGuesses(newGuesses);
+  }
+
+  return <div className="game-wrapper">
+    <GuessList guesses={guesses}/>
+    <GuessingInput updateGuesses={updateGuesses}/>
+  </div>;
 }
 
 export default Game;
